a8dcbb26dfea3b2a077dab11388c804ed3e42f7a
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-08 14:13:07 -0500

    quotes?

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res) {
-  res.send("Hello Express")
+  res.send('Hello Express')
 });
 
 if (!process.env.DISABLE_XORIGIN) {
