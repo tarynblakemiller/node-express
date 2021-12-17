2c65dc83a8d6a0ebbbaa88425aa1a4dec35cae8a
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-17 13:56:17 -0600

    added env

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var env = require('dotenv').config();
 
 app.use("/public", express.static(__dirname + "/public"))
 
@@ -8,7 +9,12 @@ app.get('/', function(req, res) {
   });
   
   app.get('/json', function(req, res) {
-    res.json({message: "Hello json"})
+    if(process.env.MESSAGE_STYLE == 'uppercase') {
+      response = "Hello World".toUpperCase();
+    } else {
+      response = "Hello World";
+    }  
+    console.log(response);
   })
 
 
