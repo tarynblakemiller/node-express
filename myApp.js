90f57cc698471aa3aaa5d7213bb8f4e286d88062
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-17 13:43:04 -0600

    removed other path

--- myApp.js
+++ myApp.js
@@ -3,9 +3,9 @@ var app = express();
 
 app.use('/public', express.static('__dirname + /public'))
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-  });
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/views/index.html');
+//   });
   
 
 
