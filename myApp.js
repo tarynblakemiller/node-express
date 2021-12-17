0ce209f5ff694902369e262f9f9a78e455c14654
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-17 13:46:13 -0600

    fixed syntax error

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use("/public", express.static('__dirname + /public'))
+app.use("/public", express.static(__dirname + "/public"))
 
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + '/views/index.html');
-//   });
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+  });
   
 
 
