0d8ceb37b0babfc67dd25d6975c2dcf1f9309fc9
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-20 10:28:21 -0600

    added logger middleware

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 var express = require("express");
+const req = require("express/lib/request");
 var app = express();
 var env = require("dotenv").config();
 
-app.use()
+var string = req.method + " " + req.path + " - " + req.ip;
+
+app.use(function (req, res, next) {
+  console.log(string);
+  next();
+});
 
 app.use("/public", express.static(__dirname + "/public"));
 
